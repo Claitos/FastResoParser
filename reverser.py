ae1d5649e471ac98f93d1a1bc594e02cf02a264e
Claitos <tim.weinreich@stud.uni-heidelberg.de>
2025-06-25 12:16:58 +0200

    resolved bug in file names and added new file to gitignore

--- reverser.py
+++ reverser.py
@@ -1,10 +1,10 @@
 # Read the file and reverse the lines
-with open("decays_PDG2016Plus_massorder_new.dat.txt", "r") as file:
+with open("decays_PDG2016Plus_massorder_new.dat", "r") as file:
     lines = file.readlines()
 
 # Reverse the list of lines
 lines.reverse()
 
 # Write the reversed lines to a new file
-with open("decays_PDG2016Plus_massorder_reversed.dat.txt", "w") as file:
+with open("decays_PDG2016Plus_massorder_reversed.dat", "w") as file:
     file.writelines(lines)
