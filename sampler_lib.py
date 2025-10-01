9de2e275c31a191536cc9f2fd031225a7c5ae9cf
Claitos <tim.weinreich@stud.uni-heidelberg.de>
2025-10-01 17:13:02 +0200

    produced first data files with updated widths und masses for edge study -> 4 cases

--- sampler_lib.py
+++ sampler_lib.py
@@ -12,59 +12,52 @@ import matplotlib.pyplot as plt
 
 
 
-def mass_edge(p_df: pd.DataFrame, edge_id: str = "+") -> pd.DataFrame:
+def get_edges(p_df: pd.DataFrame, identifier: str = "mass", edge_id: str = "+", verbose: bool = False) -> pd.DataFrame:
     """
-    Calculate the mass edge and replace the original mass values. Edge is determined by adding or subtracting the error.
+    Calculate the mass edge and replace the original mass values. Edge is determined by adding or subtracting the error. Only unstable particles are affected.
 
     Parameters:
-    p_df (pd.DataFrame): DataFrame containing the mass data.
-    edge_id (str): Identifier for the mass edge, either "+" or "-".
+        p_df (pd.DataFrame): DataFrame containing the mass data.
+        identifier (str): Type of edge to calculate, either "mass", "width".
+        edge_id (str): Identifier for the mass edge, either "+" or "-".
+        verbose (bool): If True, print additional information.
 
     Returns:
-    pd.DataFrame: DataFrame with the mass edge value.
+        pd.DataFrame: DataFrame with the mass edge value.
     """
     
     if edge_id not in ["+", "-"]:
         raise ValueError("edge_id must be either '+' or '-'")
-
-    err_p = p_df["Mass Error Pos (GeV)"]
-    err_n = p_df["Mass Error Neg (GeV)"]
-
-
-    if edge_id == "+":
-        p_df["Mass (GeV)"] = p_df["Mass (GeV)"] + err_p
-    else:
-        p_df["Mass (GeV)"] = p_df["Mass (GeV)"] - err_n
-
-    return p_df
-
-
-
-def width_edge(p_df: pd.DataFrame, edge_id: str = "+") -> pd.DataFrame:
-    """
-    Calculate the width edge and replace the original width values. Edge is determined by adding or subtracting the error.
-
-    Parameters:
-    p_df (pd.DataFrame): DataFrame containing the width data.
-    edge_id (str): Identifier for the width edge, either "+" or "-".
-
-    Returns:
-    pd.DataFrame: DataFrame with the width edge value.
-    """
     
-    if edge_id not in ["+", "-"]:
-        raise ValueError("edge_id must be either '+' or '-'")
+    if identifier not in ["mass", "width"]:
+        raise ValueError("identifier must be either 'mass' or 'width'")
 
-    err_p = p_df["Width Error Pos (GeV)"]
-    err_n = p_df["Width Error Neg (GeV)"]
+    stable_particles_test = p_df[p_df["Width (GeV)"] == 0.0]
+    stable_particles = stable_particles_test[stable_particles_test["No. of decay channels"] == 1]["ID"].tolist()
+    bool_series = p_df["ID"].isin(stable_particles)
 
+    if identifier == "mass":
+        err_p = p_df["Mass Error Pos (GeV)"]
+        err_n = p_df["Mass Error Neg (GeV)"]
+    elif identifier == "width":
+        err_p = p_df["Width Error Pos (GeV)"]
+        err_n = p_df["Width Error Neg (GeV)"]
 
-    if edge_id == "+":
-        p_df["Width (GeV)"] = p_df["Width (GeV)"] + err_p
-    else:
-        p_df["Width (GeV)"] = p_df["Width (GeV)"] - err_n
+    p_df_edges = p_df.copy()   # create a copy to avoid modifying the original DataFrame
 
-    return p_df
+    if identifier == "mass":
+        if edge_id == "+":
+            p_df_edges.loc[~bool_series, "Mass (GeV)"] += err_p
+        else:
+            p_df_edges.loc[~bool_series, "Mass (GeV)"] -= err_n
+    elif identifier == "width":
+        if edge_id == "+":
+            p_df_edges.loc[~bool_series, "Width (GeV)"] += err_p
+        else:
+            p_df_edges.loc[~bool_series, "Width (GeV)"] -= err_n
+
+    return p_df_edges
+
 
 
 
@@ -73,11 +66,11 @@ def br_edge(d_df: pd.DataFrame, edge_id: str = "+") -> pd.DataFrame:
     Calculate the branching ratio edge and replace the original branching ratio values. Edge is determined by adding or subtracting the error.
     
     Parameters:
-    d_df (pd.DataFrame): DataFrame containing the branching ratio data.
-    edge_id (str): Identifier for the branching ratio edge, either "+" or "-".
+        d_df (pd.DataFrame): DataFrame containing the branching ratio data.
+        edge_id (str): Identifier for the branching ratio edge, either "+" or "-".
 
     Returns:
-    pd.DataFrame: DataFrame with the branching ratio edge value.
+        pd.DataFrame: DataFrame with the branching ratio edge value.
     """
     
     if edge_id not in ["+", "-"]:
@@ -86,10 +79,12 @@ def br_edge(d_df: pd.DataFrame, edge_id: str = "+") -> pd.DataFrame:
     err_p = d_df["BR Error Pos"]
     err_n = d_df["BR Error Neg"]
 
+    d_df_br = d_df.copy()   # create a copy to avoid modifying the original DataFrame
+
 
     if edge_id == "+":
-        d_df["BranchingRatio"] = d_df["BranchingRatio"] + err_p
+        d_df_br["BranchingRatio"] = d_df_br["BranchingRatio"] + err_p
     else:
-        d_df["BranchingRatio"] = d_df["BranchingRatio"] - err_n
+        d_df_br["BranchingRatio"] = d_df_br["BranchingRatio"] - err_n
 
-    return d_df
+    return d_df_br
